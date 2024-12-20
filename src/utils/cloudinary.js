eab00238ee6a1e2eebb8acf7485ce986444f82d5
Amaniokl <amanoutlook2003@gmail.com>
2024-12-20 23:26:20 +0530

    bug fixes in register controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,6 +18,8 @@ const uploadOnCloudinary=async (localFilePath)=>{
             }
         )
         console.log("file is uploaded on cloudinary", response.url);
+
+        fs.unlinkSync(localFilePath)
         return response;
     }
     catch(error){
