5cb7b0f6e7adb5b3b607e2b8895ba94abff53495
Amaniokl <amanoutlook2003@gmail.com>
2025-01-19 01:04:51 +0530

    solved refresh token bugs

--- src/app.js
+++ src/app.js
@@ -19,12 +19,18 @@ import userRouter from './routes/user.routes.js'
 import videoRouter from './routes/video.routes.js'
 import tweetRouter from './routes/tweet.route.js'
 import playlistRouter from './routes/playlist.route.js' 
+import subscriptionRouter from './routes/subscription.route.js'
+import likeRouter from './routes/like.route.js'
+import commentRouter from './routes/comment.route.js'
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/video", videoRouter)
 app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/subsicription", subscriptionRouter)
+app.use("/api/v1/like", likeRouter)
+app.use("/api/v1/comment", commentRouter)
 // http://localhost:8000/api/v1/users/register
 
 export {app}
